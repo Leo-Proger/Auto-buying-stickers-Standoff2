7494281b2ccccdc3b9768244d42b06c097bf6131
Leonchik228337 <leonidkrasavin4@gmail.com>
2023-03-19 17:09:20 +0300

    add comments write info in file README.md

--- ForSO2.py
+++ ForSO2.py
@@ -1,5 +1,4 @@
 import asyncio
-import time
 
 import cv2
 import numpy
@@ -9,35 +8,60 @@ import win32con
 from PIL import ImageGrab
 
 
-async def check_lot_edges(screen, y1, y2):
-	global continue_, count
-	borders = screen[y1:y1 + 31, 0:-1]
-	edges = await asyncio.to_thread(cv2.Canny, borders, 400, 300)
-
-	non_zero_pixels = cv2.countNonZero(edges)
-	if non_zero_pixels > 5:
-		win32api.SetCursorPos((1390, y2))
-		win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-		win32api.SetCursorPos((BUY_X, BUY_Y))
-		await asyncio.sleep(0.05)
-		win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-		continue_ = False
-		print("Куплено!")
-	else:
-		if count == 2000:
-			pyautogui.doubleClick(761, 306, interval=0.1)
-			count = 0
-		# await asyncio.sleep(0.05)
-		else:
-			count += 1
-
-
-async def main(count_stickers):
+async def test_stickers(count_stickers):
 	bbox = {
 		1: (1155, 346, 1185, 941),
 		2: (1125, 346, 1155, 941),
 		3: (1095, 346, 1125, 941),
 		4: (1065, 346, 1095, 941),
+		}[count_stickers]
+
+	while True:
+		screen = numpy.array(await asyncio.to_thread(ImageGrab.grab, bbox=bbox))
+		edges = await asyncio.to_thread(cv2.Canny, screen, 400, 300)
+		cv2.imshow('screen', edges)
+		cv2.waitKey(1)
+
+
+async def check_lot_edges(screen, y1, y2):
+	global continue_, count
+
+	# These are the coordinates of the stickers.
+	#                y1 y2       x1 x2
+	borders = screen[y1:y1 + 31, 0:-1]
+
+	# If you want, you can change the values of 400 and 300.
+	edges = await asyncio.to_thread(cv2.Canny, borders, 400, 300)
+
+	if cv2.countNonZero(edges) > 5:
+		win32api.SetCursorPos((1390, y2))
+		win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+		win32api.SetCursorPos((BUY_X, BUY_Y))
+
+		# This doesn't need to be removed because the game has a pop-up animation confirming the purchase.
+		await asyncio.sleep(0.05)
+		win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+		continue_ = False
+		print("Purchased!")
+	else:
+		# If you want, you can change the value of 2000 (~10 seconds) to something else.
+		# I couldn't come up with a better way to do it.
+		if count == 2000:
+			pyautogui.doubleClick(761, 306, interval=0.1)
+			count = 0
+			# If you have a good and stable internet connection, you can remove or comment out this line of code.
+			await asyncio.sleep(0.05)
+		else:
+			count += 1
+
+
+async def main(count_stickers):
+	# Please read the requirements regarding this.
+	bbox = {
+		1: (1155, 346, 1185, 941),  # These are the coordinates of the first sticker.
+		2: (1125, 346, 1155, 941),  # These are the coordinates of the second sticker.
+		3: (1095, 346, 1125, 941),  # These are the coordinates of the third sticker.
+		4: (1065, 346, 1095, 941),  # These are the coordinates of the fourth sticker.
 		}[count_stickers]
 
 	while continue_:
@@ -56,6 +80,8 @@ async def main(count_stickers):
 
 
 count = 0
+# Please read the requirements regarding this.
 BUY_X, BUY_Y = 1072, 639
 continue_ = True
-asyncio.run(main(4))
+asyncio.run(main(4))  # This is the number of stickers you want to buy with the skin.
+# asyncio.run(test_stickers(1))
---
+++ README.md
@@ -0,0 +1,53 @@
+**English**:
+
+Hello, I have created a program that automatically purchases skins with stickers in the game **Standoff 2** in the
+shortest
+possible time (1 ms) (I don't know how to speed up the code even more).
+
+This program works on **Windows 10 Pro** and has not
+been tested on other operating systems. To check if the coordinates are correctly defined, remove the hashtag in the
+last line of the code. If you have a different screen resolution or something does not meet the requirements, you will
+need to change the coordinates of the stickers.
+
+**I strongly urge you to review the code and read the comments in it.**
+
+**Requirements:**
+
+1. You need to have **Python**, which can be installed from the link: _https://www.python.org/downloads/_
+2. You need to install modules for the program to work: Press **Win+R**, type **cmd** in the window that appears, and
+   then enter
+   the following commands in the command prompt: **pip install opencv-python numpy pyautogui pywin32 Pillow**.
+3. The monitor screen resolution should be **1920x1080**.
+4. The emulator must be **LdPlayer5** (I made this program using it).
+5. In the emulator settings, the resolution should be set to **1280x960**.
+6. The emulator itself should be deployed in fullscreen mode **(not F11)**.
+
+If you have any questions, please write to me on Telegram - _https://t.me/Leo_228_337_.
+I will be happy to answer 😁.
+
+**Русский**:
+
+Здравствуйте, я создал программу, которая автоматически покупает скины с наклейками в игре **Standoff 2** за кратчайшее
+возможное время (1 мс) (я не знаю, как ускорить код еще больше).
+
+Эта программа работает на **Windows 10 Pro** и не была протестирована на других операционных системах. Чтобы проверить
+правильно ли определены координаты, в коде уберите решетку в последней строчке. Если у вас другое разрешение экрана или
+что-либо не соответствует требованиям, то вам придется менять координаты наклеек.
+
+**Я настоятельно
+рекомендую вам ознакомиться с кодом и прочитать комментарии в нём.**
+
+**Требования**:
+
+1. Вам нужно установить Python, который можно загрузить по ссылке: _https://www.python.org/downloads/_.
+2. Вам нужно установить модули для работы программы: нажмите **Win+R**, введите **cmd** в появившемся окне, а затем
+   выполните
+   следующие команды в командной строке: **pip install opencv-python numpy pyautogui pywin32 Pillow**.
+3. Разрешение экрана монитора должно быть **1920x1080**.
+4. Эмулятор должен быть **LdPlayer5** (я использовал его для создания этой программы).
+5. В настройках эмулятора разрешение должно быть установлено на **1280x960**.
+6. Окно эмулятора должно быть растянуто на весь экран **(не в режиме полного экрана)**.
+
+Если у вас есть какие-либо вопросы, пожалуйста, напишите мне в **Telegram** - _https://t.me/Leo_228_337_. Я буду рад
+ответить
+😁.
